31cacb35eedc18e03a3279bbb448c127f59b0b74
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-08-30 07:21:09 +0430

    Create config.php

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+/*
+کانال شهر سورس مرجع انواع سورس کد های مختلف
+بانک انواع سورس کد های مختلف به صورت کاملا تست شده
+هر روز کلی سورس کد و اسکریپت منتظر شماست !
+
+@ShahreSource
+https://t.me/ShahreSource
+*/
+error_reporting(0);
+$user='fvsfe2lbnrqqrhnd';//یوزرنیم لازم برای کانکت به دیتابیس
+$pass='bcczjljkz6z2w8vn';//رمز لازم برای کانکت به دیتابیس
+$db='hycxmcm519fkd602';//نام دیتابیس;
+$conn=new mysqli('x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',$user,$pass,$db);
+define('API_KEY','1116092338:AAH4QzrmzhRzIoVd1lF6sJx-_Fl_q6B-rQA');//توکن ربات
+$sudo= 835459207;//شناسه عددی ادمین
+$channel= '@MELODYKA';//یوزرنیم کانال همراه با @ برای جوین اجباری یا شناسه عددی کانال(میتونید چندتا کانال تنظیم کنید)
+$userbot= 'MELODYKABot';//یوزرنیم ربات بدون @ وارد کنید
+$music_picture = '';//لینک عکس گذاشته شده در فایل زیپ
+/*
+کانال شهر سورس مرجع انواع سورس کد های مختلف
+بانک انواع سورس کد های مختلف به صورت کاملا تست شده
+هر روز کلی سورس کد و اسکریپت منتظر شماست !
+
+@ShahreSource
+https://t.me/ShahreSource
+*/
+
+
